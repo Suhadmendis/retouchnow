919878c6bf62227e2b4d41f7b5b08fdf8978c10e
AkilaMendis <akila@stacktech.io>
2021-08-13 18:40:14 +0530

    header dev

--- _js/script.js
+++ _js/script.js
@@ -48,3 +48,11 @@ var app = new Vue({
     },
   },
 });
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
