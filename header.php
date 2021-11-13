219f4777c2a6b4c3062576eb9d6056c39de489e7
Akila Mendis <akila@stacktech.io>
2021-11-14 02:15:43 +0530

    up

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+    
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
           <a class="navbar-brand" href="index.php">
